89a1f70c2d92410415dfe4d858e06979dabc817a
jjwon2149 <jjwon2149@gmail.com>
2024-10-07 14:36:18 +0900

    Initialize RxSwiftPracticeProject

---
+++ RxSwiftPracticeTests/RxSwiftPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwiftPracticeTests.swift
+//  RxSwiftPracticeTests
+//
+//  Created by 정종원 on 10/7/24.
+//
+
+import Testing
+@testable import RxSwiftPractice
+
+struct RxSwiftPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
