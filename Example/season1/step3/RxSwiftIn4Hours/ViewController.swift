a34ffdf993f6fece76d7f8a5d309421418023807
jjwon2149 <jjwon2149@gmail.com>
2024-10-07 17:14:19 +0900

    feat: Observable을 사용한 input output 나누기.

--- Example/season1/step3/RxSwiftIn4Hours/ViewController.swift
+++ Example/season1/step3/RxSwiftIn4Hours/ViewController.swift
@@ -35,24 +35,24 @@ class ViewController: UIViewController {
         //          |
         // pw input +--> check valid --> bullet
         
-        idField.rx.text.orEmpty
-        //            .filter { $0 != nil }
-        //            .map { $0! } -> .orEmpty로 변경 가능
-            .map(checkEmailValid)
-            .subscribe(onNext: { s in
-                self.idValidView.isHidden = s
-            })
-            .disposed(by: disposeBag)
+        // input: id, pw 입력
+        let idInputOb: Observable<String> = idField.rx.text.orEmpty.asObservable()
+        let idValidOb = idInputOb.map(checkEmailValid)
+
+        let pwInputOb: Observable<String> = pwField.rx.text.orEmpty.asObservable()
+        let pwValidOb = pwInputOb.map(checkPasswordValid)
         
-        pwField.rx.text.orEmpty
-            .map(checkPasswordValid)
-            .subscribe(onNext: { s in
-                self.pwValidView.isHidden = s
-            })
-            .disposed(by: disposeBag)
+        // output: 로그인버튼 enable/ id,pw 정규식 검사
+        idValidOb.subscribe(onNext: { b in
+            self.idValidView.isHidden = b
+        })
+        .disposed(by: disposeBag)
         
+        pwValidOb.subscribe(onNext: { b in
+            self.pwValidView.isHidden = b
+        })
+        .disposed(by: disposeBag)
         
-        //MARK: - combineLatest: 다른 Observable을 결합시켜 새로운 Observable을 생성해야 한다
         Observable.combineLatest(idField.rx.text.orEmpty.map(checkEmailValid), pwField.rx.text.orEmpty.map(checkPasswordValid),
             resultSelector: {s1, s2 in s1 && s2}
         )
@@ -60,6 +60,41 @@ class ViewController: UIViewController {
             self.loginButton.isEnabled = b
         })
         .disposed(by: disposeBag)
+        
+        Observable.combineLatest(idValidOb, pwValidOb,
+            resultSelector: { $0 && $1}
+        )
+        .subscribe(onNext: { b in
+            self.loginButton.isEnabled = b
+        })
+        .disposed(by: disposeBag)
+        
+        
+//        idField.rx.text.orEmpty
+//        //            .filter { $0 != nil }
+//        //            .map { $0! } -> .orEmpty로 변경 가능
+//            .map(checkEmailValid)
+//            .subscribe(onNext: { s in
+//                self.idValidView.isHidden = s
+//            })
+//            .disposed(by: disposeBag)
+//        
+//        pwField.rx.text.orEmpty
+//            .map(checkPasswordValid)
+//            .subscribe(onNext: { s in
+//                self.pwValidView.isHidden = s
+//            })
+//            .disposed(by: disposeBag)
+//        
+//        
+//        //MARK: - combineLatest: 다른 Observable을 결합시켜 새로운 Observable을 생성해야 한다
+//        Observable.combineLatest(idField.rx.text.orEmpty.map(checkEmailValid), pwField.rx.text.orEmpty.map(checkPasswordValid),
+//            resultSelector: {s1, s2 in s1 && s2}
+//        )
+//        .subscribe(onNext: { b in
+//            self.loginButton.isEnabled = b
+//        })
+//        .disposed(by: disposeBag)
     }
     
     // MARK: - Logic
