b7d4a44e632eac5c7684018b249a288a422ce4c7
jjwon2149 <jjwon2149@gmail.com>
2024-10-09 16:41:59 +0900

    chore: 로그인성공시 이동할 뷰 생성

---
+++ RxPrac/RxPrac/LoginSuccessViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginSuccessViewController.swift
+//  RxPrac
+//
+//  Created by 정종원 on 10/9/24.
+//
+
+import UIKit
+
+class LoginSuccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
