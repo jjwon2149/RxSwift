39d3687281d0d40cc8c513cbbb46438d506d981e
jjwon2149 <jjwon2149@gmail.com>
2024-10-16 23:17:37 +0900

    feat: add Custom Cell

---
+++ Pin&FlexLayoutWithRxSwiftExample/Pin&FlexLayoutWithRxSwiftExample/View/NewsTableViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  NewsTableViewCell.swift
+//  Pin&FlexLayoutWithRxSwiftExample
+//
+//  Created by 정종원 on 10/16/24.
+//
+
+import UIKit
+import PinLayout
+import FlexLayout
+
+
+class NewsTableViewCell: UITableViewCell {
+    // MARK: - Properties
+    static let identifier = "NewsTableViewCell"
+    
+    // MARK: - UIComponents
+    private let titleLabel = UILabel()
+    private let descriptionLabel = UILabel()
+    private let containerView = UIView()
+    
+    // MARK: - LifeCycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(containerView)
+        containerView.flex.direction(.column).padding(10).define { flex in
+            flex.addItem(titleLabel).marginBottom(5)
+            flex.addItem(descriptionLabel)
+        }
+        
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 10)
+        titleLabel.numberOfLines = 0
+        titleLabel.textColor = .black
+        
+        descriptionLabel.font = UIFont.systemFont(ofSize: 8)
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textColor = .gray
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        containerView.pin.all()
+        containerView.flex.layout(mode: .adjustHeight)
+    }
+    
+    func configure(with news: News) {
+        titleLabel.text = news.title
+        descriptionLabel.text = news.description
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
