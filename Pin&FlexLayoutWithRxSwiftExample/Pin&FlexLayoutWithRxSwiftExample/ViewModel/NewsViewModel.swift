831532b4ca8a86ac29ab7f8359d2f55e11d2da61
jjwon2149 <jjwon2149@gmail.com>
2024-10-16 23:19:37 +0900

    feat: add NewsViewModel - 뉴스 API로 가져온 데이터를 BehaviorSubject인 news에 넣어줌.

--- Pin&FlexLayoutWithRxSwiftExample/Pin&FlexLayoutWithRxSwiftExample/ViewModel/NewsViewModel.swift
+++ Pin&FlexLayoutWithRxSwiftExample/Pin&FlexLayoutWithRxSwiftExample/ViewModel/NewsViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class NewsViewModel {
+    // OutPut: View에 전달할 데이터 스트림
+    let news: BehaviorSubject<[News]> = BehaviorSubject(value: [])
+    let disposeBag = DisposeBag()
+    let apiKey = Bundle.main.object(forInfoDictionaryKey: "NEWS_API_KEY") as! String
+    
+    init() {
+        fetchNews()
+    }
+    
+    func fetchNews() {
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=\( apiKey)")!
+        
+        URLSession.shared.rx.data(request: URLRequest(url: url))
+            .map { data -> [News] in
+                let response = try JSONDecoder().decode(NewsResponse.self, from: data)
+                return response.articles
+            }
+            .bind(to: news)
+            .disposed(by: disposeBag)
+    }
+}
