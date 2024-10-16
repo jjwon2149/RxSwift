7182c44953b5ad8efaa08ad13a2e6a8a60737452
jjwon2149 <jjwon2149@gmail.com>
2024-10-16 23:18:54 +0900

    feat: add NewsViewController - Pin & Flex LayOut을 사용한 레이아웃 설정 - viewModel의 new를 바인딩 하여 테이블뷰 연결

--- Pin&FlexLayoutWithRxSwiftExample/Pin&FlexLayoutWithRxSwiftExample/View/NewsViewController.swift
+++ Pin&FlexLayoutWithRxSwiftExample/Pin&FlexLayoutWithRxSwiftExample/View/NewsViewController.swift
@@ -6,15 +6,63 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import PinLayout
+import FlexLayout
 
 class NewsViewController: UIViewController {
-
+    
+    // MARK: - Perperties
+    private let viewModel = NewsViewModel()
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - UIComponents
+    private lazy var tableView =  {
+        let table = UITableView()
+        table.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
+        table.estimatedRowHeight = 100
+        table.rowHeight = UITableView.automaticDimension
+        return table
+    }()
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.view.backgroundColor = .red
+        setupUI()
+        bindViewModel()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        view.pin.all()
+        view.flex.layout()
+    }
+    
+    // MARK: - Methods
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(tableView)
+        
+        view.flex.define { flex in
+            flex.addItem(tableView).grow(1)
+        }
+        
+        view.flex.layout()
+    }
+    
+    private func bindViewModel() {
+        viewModel.news
+            .observe(on: MainScheduler.instance)
+            .do(onNext: { [weak self] _ in
+                self?.tableView.reloadData()
+            })
+            .bind(to: tableView.rx.items(cellIdentifier: NewsTableViewCell.identifier,
+                                         cellType: NewsTableViewCell.self)) { index, item, cell in
+                cell.configure(with: item)
+            }
+            .disposed(by: disposeBag)
+    }
 }
 
